7fb2ff46108905bcbd08cd369594bf5d2cf5f808
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-12-02 01:42:01 +0530

    A

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import {Link} from 'react-router-dom'
-import {FaSearch, FaShoppingBag, FaSignInAlt, FaUser} from "react-icons/fa";
+import {FaSearch, FaShoppingBag, FaSignInAlt, FaUser, FaSignOutAlt} from "react-icons/fa";
+import {useState} from "react";
 
-const user = {_id: "gdfg", role: "user"};
+const user = {_id: "gdfg", role: "admin"};
 
 const Header = () => {
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <nav>
+    <nav className="header">
       <Link to={"/"}>Home</Link>
       <Link to={"/search"}>
         <FaSearch />
@@ -16,16 +20,16 @@ const Header = () => {
 
       {user?._id ? (
         <>
-          <button>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
             <FaUser />
           </button>
-          <dialog>
+          <dialog open={isOpen}>
             <div>
               {user.role === "admin" && (
                 <Link to="/admin/dashboard">Admin</Link>
               )}
               <Link to="/orders">Orders</Link>
-
+              <button><FaSignOutAlt /></button>
             </div>
           </dialog>
         </>
